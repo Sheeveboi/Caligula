7aed533f1f4b85358c592e4ca783c0e14ad9dd59
Alto Wisdom <sampmanjoe@gmail.com>
2025-12-20 06:11:21 -0600

    created setup for oppwatch database

---
+++ functions/oppwatch/patindex.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION "patindex"( "pattern" VARCHAR, "expression" VARCHAR ) RETURNS INT AS $BODY$
+SELECT
+    COALESCE(
+        STRPOS(
+             $2
+            ,(
+                SELECT
+                    ( REGEXP_MATCHES(
+                        $2
+                        ,'(' || REPLACE( REPLACE( TRIM( $1, '%' ), '%', '.*?' ), '_', '.' ) || ')'
+                        ,'i'
+                    ) )[ 1 ]
+                LIMIT 1
+            )
+        )
+        ,0
+    )
+;
+$BODY$ LANGUAGE 'sql' IMMUTABLE;
---
+++ functions/oppwatch/setup.sql
@@ -0,0 +1,59 @@
+-- drop table players;
+create table players(
+
+	username text not null,
+	threat int not null,
+	
+	img_url text,
+	notes text,
+	nation text,
+	 
+	check (LENGTH(notes) <= 2000),
+	check (threat <= 3),
+	check (
+		(
+			patindex('%http://', img_url) != 1 OR 
+			patindex('%https://', img_url) != 1
+		) 
+		AND (LENGTH(img_url) <= 150)
+	),
+	
+	primary key (username)
+	
+);
+
+-- drop table nations;
+create table nations(
+
+	nation_name text not null,
+	threat int not null,
+	
+	img_url text,
+	notes text,
+	 
+	check (LENGTH(notes) <= 2000),
+	check (threat <= 3),
+	check (
+		(
+			patindex('%http://', img_url) != 1 OR 
+			patindex('%https://', img_url) != 1
+		) 
+		AND (LENGTH(img_url) <= 150)
+	),	
+	
+	primary key (nation_name)
+	
+);
+-- drop table snitches;
+create table snitches(
+	
+	player text not null,
+	created_at timestamp not null,
+	
+	snitch_name text,
+	name_layer text,
+	nation text,
+	
+	check (LENGTH(snitch_name) <= 75),
+	check (LENGTH(name_layer) <= 75)
+);
