be5281645cfa53003df7dd67be4923ffea005d06
Alto Wisdom <sampmanjoe@gmail.com>
2025-12-19 22:02:40 -0600

    bugfixing

--- functions/oppwatch_control/remove_input_channel.sql
+++ functions/oppwatch_control/remove_input_channel.sql
@@ -1,25 +1,26 @@
-DELETE FROM input_channels WHERE channel_id = %s;
-
 DELETE FROM relay_groups (relay_group)
-SELECT %s 
+SELECT relay_group AS FROM input_channels WHERE channel_id = %s; FROM input_channels WHERE channel_id = %s;
 
 -- delete if this relay group exists
-WHERE %s IN (
+WHERE name IN (
 	SELECT relay_group from relay_groups
 )
 
 -- delete if no channenls are using this relay group
-WHERE 0 NOT IN (
+-- the input channel check here looks for one, because the channel hasnt been deleted yet
+WHERE 1 IN (
 	SELECT COUNT(relay_group) from input_channels	
-	WHERE relay_group = %s
-	
-	UNION
-	
-	SELECT COUNT(relay_group) from output_channels	
-	WHERE relay_group = %s
-	
-	UNION
-	
-	SELECT COUNT(relay_group) from alert_channels	
-	WHERE relay_group = %s
+	WHERE relay_group = name
 );
+
+WHERE 0 IN (
+	SELECT COUNT(relay_group) from output_channels	
+	WHERE relay_group = name
+);
+
+WHERE 0 IN (
+	SELECT COUNT(relay_group) from alert_channels	
+	WHERE relay_group = name
+);
+
+DELETE FROM input_channels WHERE channel_id = %s;
