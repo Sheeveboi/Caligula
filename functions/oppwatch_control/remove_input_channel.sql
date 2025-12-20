0e042acad651cb76b0a44923f544097c777e9b9f
Alto Wisdom <sampmanjoe@gmail.com>
2025-12-19 21:47:29 -0600

    auto delete relay group if no channels are using it

--- functions/oppwatch_control/remove_input_channel.sql
+++ functions/oppwatch_control/remove_input_channel.sql
@@ -1,1 +1,25 @@
 DELETE FROM input_channels WHERE channel_id = %s;
+
+DELETE FROM relay_groups (relay_group)
+SELECT %s 
+
+-- delete if this relay group exists
+WHERE %s IN (
+	SELECT relay_group from relay_groups
+)
+
+-- delete if no channenls are using this relay group
+WHERE 0 NOT IN (
+	SELECT COUNT(relay_group) from input_channels	
+	WHERE relay_group = %s
+	
+	UNION
+	
+	SELECT COUNT(relay_group) from output_channels	
+	WHERE relay_group = %s
+	
+	UNION
+	
+	SELECT COUNT(relay_group) from alert_channels	
+	WHERE relay_group = %s
+);
